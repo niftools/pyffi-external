b20489e5ef7a9141d64dbadba3a244dc518260eb
Amorilia <amorilia@users.sourceforge.net>
2011-02-05 18:18:25 +0000

    Patch batch files bugfix.

--- apply_patch.bat
+++ apply_patch.bat
@@ -2,4 +2,4 @@
 rem
 rem Usage: apply_patch.bat old_file new_file patch_file
 rem
-xdelta3.0z.x86-32.exe -d -s %1 %3 %2
+%~dp0\xdelta3.0z.x86-32.exe -d -s %1 %3 %2
--- make_patch.bat
+++ make_patch.bat
@@ -2,4 +2,4 @@
 rem
 rem Usage: make_patch.bat old_file new_file patch_file
 rem
-xdelta3.0z.x86-32.exe -e -s %1 %2 %3
+%~dp0\xdelta3.0z.x86-32.exe -e -s %1 %2 %3
