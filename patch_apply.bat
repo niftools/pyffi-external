0f657852ee1e500be732d70ecdd0ae151ef30def
Amorilia <amorilia@users.sourceforge.net>
2011-02-05 18:21:56 +0000

    Updated docs for batch files.

--- patch_apply.bat
+++ patch_apply.bat
@@ -1,5 +1,5 @@
 @echo off
 rem
-rem Usage: apply_patch.bat old_file new_file patch_file
+rem Usage: patch_apply.bat old_file new_file patch_file
 rem
 %~dp0\xdelta3.0z.x86-32.exe -d -s %1 %3 %2
--- patch_make.bat
+++ patch_make.bat
@@ -1,5 +1,5 @@
 @echo off
 rem
-rem Usage: make_patch.bat old_file new_file patch_file
+rem Usage: patch_make.bat old_file new_file patch_file
 rem
 %~dp0\xdelta3.0z.x86-32.exe -e -s %1 %2 %3
